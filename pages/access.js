24a9d98f5251336ad38db82842534aa9c4be7927
Naman Ranawat <namanranawat159@gmail.com>
2021-06-13 15:35:04 +0530

    Heart Rate data fetching successful

--- pages/access.js
+++ pages/access.js
@@ -1,19 +1,36 @@
 import { useRouter } from "next/router";
+import axios from "axios";
 
 function Access() {
   const router = useRouter();
 
-  //Get Access Token & User ID
+  // Get Access Token & User ID
   const query = router.asPath; //To get the query string
   var splitQuery = query.split("=");
   var accessToken = splitQuery[1];
   var userId = splitQuery[2];
 
-  //if statement to deal with the error of empty string (dont why that is happens occasionally, maybe a machine problem.)
+  // if statement to deal with the error of empty string (dont why that is happens occasionally, maybe a machine problem.)
   if (accessToken != undefined && userId != undefined) {
     accessToken = accessToken.split("&")[0];
     userId = userId.split("&")[0];
   }
+
+  // Fetch data from Fitbit API using the accessToken & the userId
+  axios
+    .get(
+      "https://api.fitbit.com/1/user/" +
+        userId +
+        "/activities/heart/date/today/1w.json",
+      { headers: { Authorization: "Bearer " + accessToken } }
+    )
+    .then(res => {
+      const heartData = res.data;
+      console.log(heartData);
+    })
+    .catch(error => {
+      console.log("error " + error);
+    });
 
   console.log(splitQuery);
   console.log(accessToken);

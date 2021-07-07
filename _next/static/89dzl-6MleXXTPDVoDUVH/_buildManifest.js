75cc067ff9011821a8215f555db699afb8b34050
naman159 <namanranawat159@gmail.com>
2021-07-07 15:02:41 +0000

    Deploying to gh-pages from @ DACSS/Health-Survey@2d319336745677bd7ded01409428ecd4d6847baf 🚀

---
+++ _next/static/89dzl-6MleXXTPDVoDUVH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9224a5561f67890bd22b.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],"/access":["static/chunks/pages/access-17c91760318bca527bc4.js"],sortedPages:["/","/_app","/_error","/access"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

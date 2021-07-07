2d319336745677bd7ded01409428ecd4d6847baf
Naman Ranawat <namanranawat159@gmail.com>
2021-07-07 20:31:49 +0530

    Requesting hr & activity

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   const authUrl =
     process.env.NEXT_PUBLIC_REDIRECT_URL ||
-    "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=23B8VC&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Faccess&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800";
+    "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=23B8VC&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Faccess&scope=activity%20heartrate&expires_in=604800https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=23B8VC&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Faccess&scope=activity%20heartrate&expires_in=604800";
 
   return (
     <center>
